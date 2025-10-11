187f30c8d1182d267486fde59115596182b451f7
Nathan De Pachtere <nathan.depachtere@alpsify.com>
2025-10-11 10:16:49 +0200

    Add default component for LayoutKufe

--- libs/vue/layouts/kufe/LayoutKufe.vue
+++ libs/vue/layouts/kufe/LayoutKufe.vue
@@ -1,7 +1,42 @@
 <template>
-  <LayoutKufeRoot> </LayoutKufeRoot>
+  <LayoutKufeRoot>
+    <LayoutKufeHeader><slot name="header" /></LayoutKufeHeader>
+    <LayoutKufeNav>
+      <slot name="nav" />
+      <LayoutKufeSheetMobile class="lg:hidden">
+        <template #trigger>
+          <Button
+            icon-only
+            variant="ghost"
+          >
+            <template #icon>
+              <IconMenu />
+            </template>
+          </Button>
+        </template>
+        <template #content>
+          <slot name="sheetMobileContent" />
+        </template>
+      </LayoutKufeSheetMobile>
+    </LayoutKufeNav>
+    <LayoutKufeMain>
+      <slot name="main" />
+    </LayoutKufeMain>
+    <LayoutKufeFooter>
+      <slot name="footer" />
+    </LayoutKufeFooter>
+  </LayoutKufeRoot>
 </template>
 
 <script setup lang="ts">
-import LayoutKufeRoot from './LayoutKufeRoot.vue';
+import IconMenu from '@lychen/vue-icons/IconMenu.vue';
+import {
+  LayoutKufeFooter,
+  LayoutKufeHeader,
+  LayoutKufeMain,
+  LayoutKufeNav,
+  LayoutKufeRoot,
+} from '.';
+import LayoutKufeSheetMobile from './LayoutKufeSheetMobile.vue';
+import Button from '@lychen/vue-components-core/button/Button.vue';
 </script>
