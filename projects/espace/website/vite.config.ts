b0e9eb839466c86dd0ae084087135dcedfab4e6b
Nathan De Pachtere <nathan.depachtere@alpsify.com>
2025-11-15 19:01:09 +0100

    ADd base files for the website

---
+++ projects/espace/website/vite.config.ts
@@ -0,0 +1,41 @@
+import path from 'node:path';
+
+import type { UserConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import mkcert from 'vite-plugin-mkcert';
+import generateSitemap from 'vite-ssg-sitemap';
+import tailwindcss from '@tailwindcss/vite';
+
+const config: UserConfig = {
+  server: {
+    https: {},
+    port: 5145,
+  },
+  plugins: [
+    tailwindcss(),
+    mkcert({
+      hosts: ['esapce.lychen.local'],
+    }),
+    vue({
+      template: {
+        transformAssetUrls: {
+          includeAbsolute: false,
+        },
+      },
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  ssgOptions: {
+    script: 'async',
+    formatting: 'prettify',
+    onFinished() {
+      generateSitemap({ hostname: `https://${process.env.VITE_UNHEAD_HOST}` });
+    },
+  },
+};
+
+export default config;
