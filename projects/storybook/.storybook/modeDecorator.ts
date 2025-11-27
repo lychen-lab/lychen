1a2a18cb4daa6d178849bf0fc7421bee3a957dca
Nathan De Pachtere <nathan.depachtere@alpsify.com>
2025-11-27 21:02:56 +0100

    storybook - Responsive modeDecorator

--- projects/storybook/.storybook/modeDecorator.ts
+++ projects/storybook/.storybook/modeDecorator.ts
@@ -1,18 +1,18 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { onMounted, onUnmounted, ref, watch } from 'vue';
 import type { Decorator } from '@storybook/vue3';
-import TooltipProvider from '@lychen/vue-components-core/tooltip/TooltipProvider.vue';
+import TooltipProvider from '@swisssteel/vue-components-core/tooltip/TooltipProvider.vue';
 
 export function ModeDecorator(story: any, context: any): any {
   return {
     components: { Story: story(), TooltipProvider },
     template: `
       <TooltipProvider>
-        <div class="grid grid-cols-[1fr_1fr] gap-4 story-wrapper">
-          <div class="flex flex-row items-center justify-center bg-surface text-on-surface p-4 rounded-md relative min-h-[100px]" data-theme="light">
+        <div class="flex flex-row gap-4 story-wrapper flex-wrap">
+          <div class="flex flex-row items-center justify-center bg-surface text-on-surface p-4 rounded-md relative min-h-[100px] grow" data-theme="light">
             <Story />
           </div>
-          <div class="flex flex-row items-center justify-center bg-surface text-on-surface p-4 rounded-md relative min-h-[100px]" data-theme="dark">
+          <div class="flex flex-row items-center justify-center bg-surface text-on-surface p-4 rounded-md relative min-h-[100px] grow" data-theme="dark">
             <Story />
           </div>
         </div>
