c12dd3011a4661f88b782159f1cf002a0574f240
Nathan De Pachtere <nathan.depachtere@alpsify.com>
2025-11-29 09:36:52 +0100

    Clean DatetimeFormat

--- libs/vue/i18n/configs/DatetimeFormat.ts
+++ libs/vue/i18n/configs/DatetimeFormat.ts
@@ -1,34 +1,6 @@
 import { type I18nOptions } from 'vue-i18n';
 
-const en = {
-  long: {
-    day: '2-digit',
-    hour: 'numeric',
-    minute: 'numeric',
-    month: 'short',
-    year: 'numeric',
-  },
-  'month-year': {
-    month: 'short',
-    year: 'numeric',
-  },
-  numeric: {
-    day: '2-digit',
-    month: '2-digit',
-    year: 'numeric',
-  },
-  short: {
-    day: '2-digit',
-    month: 'short',
-    year: 'numeric',
-  },
-  time: {
-    hour: 'numeric',
-    minute: 'numeric',
-  },
-} as const;
-
-const fr = {
+const formats = {
   long: {
     day: '2-digit',
     hour: 'numeric',
@@ -57,7 +29,9 @@ const fr = {
 } as const;
 
 export const datetimeFormats: I18nOptions['datetimeFormats'] = {
-  'en-US': en,
-  'en-GB': en,
-  'fr-FR': fr,
+  'en-US': formats,
+  'en-GB': formats,
+  'fr-FR': formats,
+  fr: formats,
+  en: formats,
 };
