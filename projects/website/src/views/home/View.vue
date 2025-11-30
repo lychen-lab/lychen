87aae8874e1227e85a0ffd7f728bfe1258d529de
Nathan De Pachtere <nathan.depachtere@alpsify.com>
2025-11-30 11:48:29 +0100

    Add back home page hero

--- projects/website/src/views/home/View.vue
+++ projects/website/src/views/home/View.vue
@@ -1,9 +1,6 @@
 <template>
-  <div>
-    {{ t('meta.title') }}
-  </div>
-  <!--<PageHomeHero />
-  <SectionCrowdfunding />
+  <PageHomeHero />
+  <!--<SectionCrowdfunding />
   <PageHomeApplications />
   <PageHomeUseCases />
   <PageHomeBento />
@@ -17,6 +14,7 @@ import ogImageUrl from './assets/HomeOgImage.webp';
 import { usePrefixedI18n } from '@lychen/vue-i18n/composables/useI18nExtended';
 import { CONFIG } from './i18n';
 import { useExtendedHead } from '@lychen/vue-unhead-composables/useExtendedHead';
+import PageHomeHero from './PageHomeHero.vue';
 
 const { t } = usePrefixedI18n(CONFIG);
 
