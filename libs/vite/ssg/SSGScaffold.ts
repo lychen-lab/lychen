f78f3727ab898c362493ea62fe78e9f248d832c8
Nathan De Pachtere <nathan.depachtere@alpsify.com>
2025-12-04 17:14:09 +0100

    Clean and reorganize code to handle app without locale in URLs

--- libs/vite/ssg/SSGScaffold.ts
+++ libs/vite/ssg/SSGScaffold.ts
@@ -1,8 +1,9 @@
 import { ViteSSG } from 'vite-ssg';
 
-import { RouterView, type RouteRecordRaw } from 'vue-router';
+import { type RouteRecordRaw } from 'vue-router';
 import { useTrans } from '@lychen/vue-i18n/composables/useTrans';
 import { createI18n } from '@lychen/vue-i18n/configs/createI18n';
+import { buildConfigObject } from '@lychen/vue-router-configs/DefaultConfig';
 import { useRouteMiddleware } from '@lychen/vue-i18n/composables/useRouteMiddleware';
 
 export function ViteSSGScaffold(app: Parameters<typeof ViteSSG>[0], routes: RouteRecordRaw[]) {
@@ -10,24 +11,9 @@ export function ViteSSGScaffold(app: Parameters<typeof ViteSSG>[0], routes: Rout
 
   const i18nUtilities = useTrans(i18n);
 
-  function buildConfigObject(routes: RouteRecordRaw[], i18nUtilities: unknown) {
-    const localizedRoutes: RouteRecordRaw[] = [
-      {
-        path: '/:locale?',
-        component: RouterView,
-        beforeEnter: useRouteMiddleware(i18nUtilities).beforeEnter,
-        children: routes,
-      },
-    ];
-
-    return {
-      routes: localizedRoutes,
-    };
-  }
-
   return ViteSSG(
     app,
-    buildConfigObject(routes, i18nUtilities),
+    buildConfigObject(routes, i18nUtilities, useRouteMiddleware(i18nUtilities).beforeEnter),
     ({ app, router, routes, isClient, initialState }) => {
       app.use(i18n);
     },
--- libs/vue/router/configs/DefaultConfig.ts
+++ libs/vue/router/configs/DefaultConfig.ts
@@ -1,19 +1,30 @@
-import { RouterView, type RouteRecordRaw } from 'vue-router';
+import { RouterView, type NavigationGuardWithThis, type RouteRecordRaw } from 'vue-router';
 import { scrollBehavior } from './ScrollBehavior';
 
-export function buildConfigObject(routes: RouteRecordRaw[], i18nUtilities: unknown) {
-  const localizedRoutes: RouteRecordRaw[] = [
-    {
-      path: '/:locale?',
-      component: RouterView,
-      beforeEnter: useRouteMiddleware(i18nUtilities).beforeEnter,
-      children: routes,
-    },
-  ];
+export function buildConfigObject(
+  routes: RouteRecordRaw[],
+  i18nUtilities?: unknown,
+  beforeEnter?: NavigationGuardWithThis<undefined>,
+) {
+  if (i18nUtilities && beforeEnter) {
+    const localizedRoutes: RouteRecordRaw[] = [
+      {
+        path: '/:locale?',
+        component: RouterView,
+        beforeEnter: beforeEnter,
+        children: routes,
+      },
+    ];
+    return {
+      scrollBehavior,
+      routes: localizedRoutes,
+      base: import.meta.env.BASE_URL,
+    };
+  }
 
   return {
     scrollBehavior,
-    routes: localizedRoutes,
+    routes,
     base: import.meta.env.BASE_URL,
   };
 }
