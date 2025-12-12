18613decc68289dc89c8b32949f97bf35953de8e
Nathan De Pachtere <nathan.depachtere@lychen.org>
2025-12-12 14:54:04 +0100

    Website team view add translations (#74)

--- projects/website/src/views/team/View.vue
+++ projects/website/src/views/team/View.vue
@@ -1,26 +1,66 @@
 <template>
   <Container class="flex flex-col items-center gap-4 pt-20">
     <Title variant="h1">{{ t('title') }}</Title>
-    <Paragraph class="w-3/4 text-center opacity-60">A diverse team of passionate ...</Paragraph>
-    <div class="grid h-50 w-full grid-cols-4 grid-rows-3 gap-8">
-      <div class="row-span-2 rounded-[60px] bg-green-300"></div>
-      <div class="row-span-1 rounded-[60px] bg-green-800"></div>
-      <div class="row-span-2 rounded-[60px] bg-pink-300"></div>
-      <div class="row-span-1 rounded-[60px] bg-blue-300"></div>
-      <div class="row-span-1 rounded-[60px] bg-red-300"></div>
-      <div class="row-span-1 rounded-[60px] bg-yellow-300"></div>
+    <Paragraph class="w-3/4 text-center opacity-60">{{ t('header_subtitle') }}</Paragraph>
+    <div class="grid h-180 w-full grid-cols-4 grid-rows-3 gap-4 px-4 md:px-20">
+      <div class="row-span-2 w-full overflow-hidden rounded-[100px] bg-teal-500">
+        <img
+          src="https://images.pexels.com/photos/3184431/pexels-photo-3184431.jpeg"
+          class="h-full w-full object-cover"
+        />
+      </div>
+      <div class="row-span-1 w-full overflow-hidden rounded-[100px] bg-amber-500">
+        <img
+          src="https://images.pexels.com/photos/1685650/pexels-photo-1685650.jpeg"
+          class="h-full w-full object-cover"
+        />
+      </div>
+      <div class="row-span-2 w-full overflow-hidden rounded-[100px] bg-orange-500">
+        <img
+          class="h-full w-full object-cover"
+          src="https://images.pexels.com/photos/1172207/pexels-photo-1172207.jpeg"
+        />
+      </div>
+      <div class="row-span-1 w-full overflow-hidden rounded-[100px] bg-purple-500">
+        <img
+          class="h-full w-full scale-110 object-cover hue-rotate-15"
+          src="https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg"
+        />
+      </div>
+      <div class="row-span-2 w-full overflow-hidden rounded-[100px] bg-green-500">
+        <img
+          src="https://res.cloudinary.com/ddhvfiezg/image/upload/v1765545330/019b12b4-718c-7d11-b33e-dde66feeff83.webp"
+          class="h-full w-full object-cover"
+        />
+      </div>
+      <div class="row-span-2 w-full overflow-hidden rounded-[100px] bg-blue-500">
+        <img
+          src="https://images.pexels.com/photos/1685650/pexels-photo-1685650.jpeg"
+          class="h-full w-full object-cover"
+        />
+      </div>
+      <div class="row-span-1 w-full overflow-hidden rounded-[100px] bg-pink-500">
+        <img
+          src="https://images.pexels.com/photos/142497/pexels-photo-142497.jpeg"
+          class="h-full w-full object-cover"
+        />
+      </div>
+      <div class="row-span-2 w-full overflow-hidden rounded-[100px] bg-stone-500">
+        <img
+          src="https://images.pexels.com/photos/7583935/pexels-photo-7583935.jpeg"
+          class="h-full w-full object-cover"
+        />
+      </div>
     </div>
   </Container>
 
   <Container class="flex flex-col items-center gap-4">
-    <Title variant="h2">The lychen team</Title>
+    <Title variant="h2">{{ t('team_section.title') }}</Title>
     <Paragraph class="text-center opacity-60 md:w-3/4">
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laudantium quisquam magni maxime
-      eligendi sequi, aspernatur placeat ea, blanditiis accusantium laborum molestias quae
-      doloremque magnam ducimus quaerat repellat architecto, aliquid alias.
+      {{ t('team_section.description') }}
     </Paragraph>
     <div class="flex flex-row items-center gap-2">
-      <small>N'hésitez pas à nous contacter </small>
+      <small>{{ t('contact_label') }} </small>
 
       <Badge class="bg-stone-200"
         >{{ contactEmail }}
@@ -52,19 +92,21 @@
   </Container>
 
   <Container class="flex flex-col gap-8 md:grid md:grid-cols-2">
-    <div class="flex flex-col items-start gap-8">
-      <Title variant="h2">Découvrir notre manière de construire l'écosystème</Title>
+    <div class="flex flex-col items-start justify-center gap-8">
+      <Title variant="h2">{{ t('ecosystem_section.title') }}</Title>
       <Paragraph class="w-3/4 text-left opacity-60">
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi sint at atque illum esse
-        accusantium necessitatibus, molestias fuga voluptatem voluptatibus provident ut maxime neque
-        officia odit iusto eius illo nisi!
+        {{ t('ecosystem_section.description') }}
       </Paragraph>
       <RouterLink :to="{ name: ROUTE_CHARTER.name }">
-        <Button>Notre charte</Button>
+        <Button>{{ t('ecosystem_section.cta') }}</Button>
       </RouterLink>
     </div>
-    <div>
-      <img />
+    <div class="overflow-hidden rounded-[100px]">
+      <img
+        src="https://res.cloudinary.com/ddhvfiezg/image/upload/v1765534714/ecosystem_y3ufwm.jpg"
+        :alt="t('ecosystem_section.image_alt')"
+        class="h-full w-full object-cover"
+      />
     </div>
   </Container>
 </template>
--- projects/website/src/views/team/data/team.ts
+++ projects/website/src/views/team/data/team.ts
@@ -4,46 +4,62 @@ export default [
   {
     firstname: 'Nathan',
     lastname: 'De Pachtere',
-    role: 'Président',
-    image: 'https://res.cloudinary.com/ddhvfiezg/image/upload/v1765464918/NathanDePachtere.webp',
+    id: 'nathan_depachtere',
+    role: 'president',
+    image:
+      'https://res.cloudinary.com/ddhvfiezg/image/upload/v1765464918/019b12c1-c9bb-73d9-b409-b795b1a26f9d.webp',
     link: 'https://www.linkedin.com/in/nathandepachtere/',
     email: EMAIL.Presidence,
   },
   {
     firstname: 'Jennifer',
     lastname: 'Geoffroy',
-    role: 'Vice-Présidente',
-    image: 'https://res.cloudinary.com/ddhvfiezg/image/upload/v1765464918/JenniferGeoffroy.webp',
+    id: 'jennifer_geoffroy',
+    role: 'vice_president',
+    image:
+      'https://res.cloudinary.com/ddhvfiezg/image/upload/v1765464918/019b12c2-0402-7868-85f0-4b4379db5eb1.webp',
     email: EMAIL.Presidence,
   },
   {
     firstname: 'Nadège',
     lastname: 'Crépel',
-    image: '',
+    id: 'nadege_crepel',
+    image:
+      'https://res.cloudinary.com/ddhvfiezg/image/upload/v1765546912/019b12cc-8a55-7544-9618-86d48504a3e5.webp',
   },
   {
     firstname: 'Noëllie',
     lastname: '',
-    image: '',
+    id: 'noellie',
+    image:
+      'https://res.cloudinary.com/ddhvfiezg/image/upload/v1765547075/019b12ce-bd5e-7312-a8e6-d66d881410ed.webp',
   },
   {
     firstname: 'Luc',
     lastname: 'Auvigne',
-    image: '',
+    id: 'luc_auvigne',
+    image:
+      'https://res.cloudinary.com/ddhvfiezg/image/upload/v1765547005/019b12cd-d1d1-7ba8-9e6b-a5374a571d2f.webp',
   },
   {
     firstname: 'Justine',
     lastname: 'Delalleau',
-    image: '',
+    id: 'justine_delalleau',
+    image:
+      'https://res.cloudinary.com/ddhvfiezg/image/upload/v1765547130/019b12d1-58a5-7021-b8d0-ffd3d8f72ad9.webp',
   },
   {
     firstname: 'Thomas',
-    lastname: 'Détharré',
-    image: '',
+    lastname: '',
+    id: 'thomas',
+    image:
+      'https://res.cloudinary.com/ddhvfiezg/image/upload/v1765547128/019b12d0-b72c-7f00-9a0a-7356f7f83718.webp',
   },
   {
     firstname: 'Agnès',
     lastname: 'Balquet',
-    image: '',
+    id: 'agnes_balquet',
+    image:
+      'https://res.cloudinary.com/ddhvfiezg/image/upload/v1765547126/019b12d0-5273-7faf-9820-0befe3f270d4.webp',
   },
 ];
