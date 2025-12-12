f176958dc187fc5d05d1da1d2eab3aa56b0d2a1f
Nathan De Pachtere <nathan.depachtere@alpsify.com>
2025-12-12 14:24:12 +0100

    Add pictures

--- projects/website/src/views/team/View.vue
+++ projects/website/src/views/team/View.vue
@@ -2,13 +2,55 @@
   <Container class="flex flex-col items-center gap-4 pt-20">
     <Title variant="h1">{{ t('title') }}</Title>
     <Paragraph class="w-3/4 text-center opacity-60">{{ t('header_subtitle') }}</Paragraph>
-    <div class="grid h-50 w-full grid-cols-4 grid-rows-3 gap-8">
-      <div class="row-span-2 rounded-[60px] bg-green-300"></div>
-      <div class="row-span-1 rounded-[60px] bg-green-800"></div>
-      <div class="row-span-2 rounded-[60px] bg-pink-300"></div>
-      <div class="row-span-1 rounded-[60px] bg-blue-300"></div>
-      <div class="row-span-1 rounded-[60px] bg-red-300"></div>
-      <div class="row-span-1 rounded-[60px] bg-yellow-300"></div>
+    <div class="grid h-180 w-full grid-cols-4 grid-rows-3 gap-4 px-4 md:px-20">
+      <div class="row-span-2 w-full overflow-hidden rounded-[100px] bg-teal-500">
+        <img
+          src="https://images.pexels.com/photos/3184431/pexels-photo-3184431.jpeg"
+          class="h-full w-full object-cover"
+        />
+      </div>
+      <div class="row-span-1 w-full overflow-hidden rounded-[100px] bg-amber-500">
+        <img
+          src="https://images.pexels.com/photos/1685650/pexels-photo-1685650.jpeg"
+          class="h-full w-full object-cover"
+        />
+      </div>
+      <div class="row-span-2 w-full overflow-hidden rounded-[100px] bg-orange-500">
+        <img
+          class="h-full w-full object-cover"
+          src="https://images.pexels.com/photos/1172207/pexels-photo-1172207.jpeg"
+        />
+      </div>
+      <div class="row-span-1 w-full overflow-hidden rounded-[100px] bg-purple-500">
+        <img
+          class="h-full w-full scale-110 object-cover hue-rotate-15"
+          src="https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg"
+        />
+      </div>
+      <div class="row-span-2 w-full overflow-hidden rounded-[100px] bg-green-500">
+        <img
+          src="https://res.cloudinary.com/ddhvfiezg/image/upload/v1765545330/019b12b4-718c-7d11-b33e-dde66feeff83.webp"
+          class="h-full w-full object-cover"
+        />
+      </div>
+      <div class="row-span-2 w-full overflow-hidden rounded-[100px] bg-blue-500">
+        <img
+          src="https://images.pexels.com/photos/1685650/pexels-photo-1685650.jpeg"
+          class="h-full w-full object-cover"
+        />
+      </div>
+      <div class="row-span-1 w-full overflow-hidden rounded-[100px] bg-pink-500">
+        <img
+          src="https://images.pexels.com/photos/142497/pexels-photo-142497.jpeg"
+          class="h-full w-full object-cover"
+        />
+      </div>
+      <div class="row-span-2 w-full overflow-hidden rounded-[100px] bg-stone-500">
+        <img
+          src="https://images.pexels.com/photos/7583935/pexels-photo-7583935.jpeg"
+          class="h-full w-full object-cover"
+        />
+      </div>
     </div>
   </Container>
 
@@ -59,10 +101,11 @@
         <Button>{{ t('ecosystem_section.cta') }}</Button>
       </RouterLink>
     </div>
-    <div>
+    <div class="overflow-hidden rounded-[100px]">
       <img
         src="https://res.cloudinary.com/ddhvfiezg/image/upload/v1765534714/ecosystem_y3ufwm.jpg"
         :alt="t('ecosystem_section.image_alt')"
+        class="h-full w-full object-cover"
       />
     </div>
   </Container>
