88abc453dd30f207127c7b09ba87f0702875685e
Nathan De Pachtere <nathan.depachtere@alpsify.com>
2025-12-12 14:48:28 +0100

    Map profil picture to URL

--- projects/website/src/views/team/data/team.ts
+++ projects/website/src/views/team/data/team.ts
@@ -6,7 +6,8 @@ export default [
     lastname: 'De Pachtere',
     id: 'nathan_depachtere',
     role: 'president',
-    image: 'https://res.cloudinary.com/ddhvfiezg/image/upload/v1765464918/NathanDePachtere.webp',
+    image:
+      'https://res.cloudinary.com/ddhvfiezg/image/upload/v1765464918/019b12c1-c9bb-73d9-b409-b795b1a26f9d.webp',
     link: 'https://www.linkedin.com/in/nathandepachtere/',
     email: EMAIL.Presidence,
   },
@@ -15,43 +16,50 @@ export default [
     lastname: 'Geoffroy',
     id: 'jennifer_geoffroy',
     role: 'vice_president',
-    image: 'https://res.cloudinary.com/ddhvfiezg/image/upload/v1765464918/JenniferGeoffroy.webp',
+    image:
+      'https://res.cloudinary.com/ddhvfiezg/image/upload/v1765464918/019b12c2-0402-7868-85f0-4b4379db5eb1.webp',
     email: EMAIL.Presidence,
   },
   {
     firstname: 'Nadège',
     lastname: 'Crépel',
     id: 'nadege_crepel',
-    image: '',
+    image:
+      'https://res.cloudinary.com/ddhvfiezg/image/upload/v1765546912/019b12cc-8a55-7544-9618-86d48504a3e5.webp',
   },
   {
     firstname: 'Noëllie',
     lastname: '',
     id: 'noellie',
-    image: '',
+    image:
+      'https://res.cloudinary.com/ddhvfiezg/image/upload/v1765547075/019b12ce-bd5e-7312-a8e6-d66d881410ed.webp',
   },
   {
     firstname: 'Luc',
     lastname: 'Auvigne',
     id: 'luc_auvigne',
-    image: '',
+    image:
+      'https://res.cloudinary.com/ddhvfiezg/image/upload/v1765547005/019b12cd-d1d1-7ba8-9e6b-a5374a571d2f.webp',
   },
   {
     firstname: 'Justine',
     lastname: 'Delalleau',
     id: 'justine_delalleau',
-    image: '',
+    image:
+      'https://res.cloudinary.com/ddhvfiezg/image/upload/v1765547130/019b12d1-58a5-7021-b8d0-ffd3d8f72ad9.webp',
   },
   {
     firstname: 'Thomas',
-    lastname: 'Détharré',
-    id: 'thomas_detharre',
-    image: '',
+    lastname: '',
+    id: 'thomas',
+    image:
+      'https://res.cloudinary.com/ddhvfiezg/image/upload/v1765547128/019b12d0-b72c-7f00-9a0a-7356f7f83718.webp',
   },
   {
     firstname: 'Agnès',
     lastname: 'Balquet',
     id: 'agnes_balquet',
-    image: '',
+    image:
+      'https://res.cloudinary.com/ddhvfiezg/image/upload/v1765547126/019b12d0-5273-7faf-9820-0befe3f270d4.webp',
   },
 ];
