1404afb6a14f3fcdd0fd05f971d40e81b9fbb142
Nathan De Pachtere <nathan.depachtere@alpsify.com>
2025-12-14 09:30:29 +0100

    Add image to initiatives section

--- projects/website/src/layouts/TheNavigation.vue
+++ projects/website/src/layouts/TheNavigation.vue
@@ -10,23 +10,6 @@
       <NavigationMenu class="hidden lg:flex">
         <NavigationMenuList>
           <NavigationMenuItem>
-            <NavigationMenuTrigger>{{ t(`navigation.initiatives.title`) }}</NavigationMenuTrigger>
-            <NavigationMenuContent
-              class="bg-surface-container/70 flex flex-col gap-4 rounded-lg p-4 backdrop-blur-lg md:w-[300px]"
-            >
-              <NavigationMenuSubLink
-                :title="t('navigation.applications.title')"
-                :description="t('navigation.applications.description')"
-                :route="{ name: ROUTE_APPLICATIONS.name }"
-              />
-
-              <NavigationMenuSubLink
-                :title="t('navigation.label.title')"
-                :description="t('navigation.label.description')"
-              />
-            </NavigationMenuContent>
-          </NavigationMenuItem>
-          <NavigationMenuItem>
             <RouterLink :to="i18nRoute({ name: ROUTE_MISSION.name })">
               <NavigationMenuLink
                 as="div"
@@ -35,6 +18,31 @@
                 {{ t(`navigation.mission.title`) }}
               </NavigationMenuLink>
             </RouterLink>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <NavigationMenuTrigger>{{ t(`navigation.initiatives.title`) }}</NavigationMenuTrigger>
+            <NavigationMenuContent
+              class="bg-surface-container/70 grid grid-cols-2 gap-4 rounded-lg p-4 backdrop-blur-lg md:w-[600px]"
+            >
+              <div class="h-50 overflow-hidden rounded-lg">
+                <img
+                  src="https://res.cloudinary.com/ddhvfiezg/image/upload/v1765700848/019b1bf9-0a48-73c9-ae92-1956fc25fe64.webp"
+                  class="h-full w-full object-cover"
+                />
+              </div>
+              <div class="flex flex-col gap-4">
+                <NavigationMenuSubLink
+                  :title="t('navigation.applications.title')"
+                  :description="t('navigation.applications.description')"
+                  :route="{ name: ROUTE_APPLICATIONS.name }"
+                />
+
+                <NavigationMenuSubLink
+                  :title="t('navigation.label.title')"
+                  :description="t('navigation.label.description')"
+                />
+              </div>
+            </NavigationMenuContent>
           </NavigationMenuItem>
           <NavigationMenuItem>
             <NavigationMenuTrigger>{{ t(`navigation.about.title`) }}</NavigationMenuTrigger>
