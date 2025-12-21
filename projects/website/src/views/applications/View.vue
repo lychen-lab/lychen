c98389663e840d7898189806969ced3d002cbb43
Nathan De Pachtere <nathan.depachtere@alpsify.com>
2025-12-21 14:45:56 +0100

    Create page structure

--- projects/website/src/views/applications/View.vue
+++ projects/website/src/views/applications/View.vue
@@ -1,14 +1,58 @@
 <template>
-  <div class="flex flex-row gap-4">
-    {{ t('title') }}
+  <Container class="grid grid-cols-3 gap-4">
+    <div
+      v-for="(application, index) in titleSortedApplicationsList"
+      :key="index"
+      class="flex flex-col gap-2 rounded-3xl p-4"
+    >
+      <div class="flex items-center gap-2">
+        <Title
+          variant="h3"
+          class="lowercase"
+          >{{ application.title }}</Title
+        >
+        <Badge>{{ application.state }}</Badge>
+      </div>
 
-    - Listes des applications
-  </div>
+      <Paragraph class="opacity-80">{{ application.description }}</Paragraph>
+    </div>
+  </Container>
+
+  <Container class="flex flex-col items-center gap-4">
+    <Title
+      variant="h2"
+      class="text-center md:basis-2/5"
+      >{{ t('oss.title') }}</Title
+    >
+    <Paragraph
+      variant="website-highlight"
+      class="text-center opacity-90 md:w-3/5"
+      >{{ t('oss.description') }}</Paragraph
+    >
+    <RouterLink :to="SOCIAL_LINK.GitHub">
+      <Button variant="ghost"
+        >{{ t('oss.button.label') }}
+        <template #icon>
+          <IconArrowUpRight />
+        </template>
+      </Button>
+    </RouterLink>
+  </Container>
 </template>
 
 <script lang="ts" setup>
 import { usePrefixedI18n } from '@lychen/vue-i18n/composables/useI18nExtended';
 import { CONFIG } from './i18n';
+import { useApplicationsCatalog } from '@lychen/vue-applications/composables/useApplicationsCatalog';
+import Container from '@lychen/vue-components-website/container/Container.vue';
+import Title from '@lychen/vue-components-website/title/Title.vue';
+import Paragraph from '@lychen/vue-components-website/paragraph/Paragraph.vue';
+import Button from '@lychen/vue-components-core/button/Button.vue';
+import IconArrowUpRight from '@lychen/vue-icons/IconArrowUpRight.vue';
+import { SOCIAL_LINK } from '@lychen/typescript-constants/Social';
+import Badge from '@lychen/vue-components-core/badge/Badge.vue';
 
 const { t } = usePrefixedI18n(CONFIG);
+
+const { titleSortedApplicationsList } = useApplicationsCatalog();
 </script>
