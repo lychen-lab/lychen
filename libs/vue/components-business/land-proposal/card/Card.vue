aecc5e5d46d959b5997879ae44fe90ec8d45a888
Nathan De Pachtere <nathan.depachtere@alpsify.com>
2025-12-30 11:34:55 +0100

    add fake datas and complet homepage

--- libs/vue/components-business/land-proposal/card/Card.vue
+++ libs/vue/components-business/land-proposal/card/Card.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="bg-surface-container-low border-on-surface/5 max-w-[300px] rounded-3xl border-1 p-2"
+    class="bg-surface-container-low border-on-surface/5 flex max-w-[275px] min-w-[275px] flex-col rounded-3xl border-1 p-2 text-left"
     :data-uuid="uuid"
     :class="clickable ? 'cursor-pointer hover:shadow-md' : 'cursor-default'"
   >
@@ -13,46 +13,40 @@
         decoding="async"
       />
       <Button
-        class="bg-surface/5 absolute top-2 right-2 backdrop-blur-sm"
+        class="bg-surface text-on-surface absolute top-2 right-2"
         icon-only
-        size="sm"
+        size="xs"
         @click="$emit('favorite')"
         ><template #icon
-          ><IconHeart v-if="!isFavorite" /><IconHeartFilled
+          ><IconHeart
+            v-if="!isFavorite"
+            class="!size-3" /><IconHeartFilled
             v-else
-            class="text-red-700" /></template
+            class="!size-3 text-red-700" /></template
       ></Button>
       <div
         v-if="displayCity && city"
-        class="absolute bottom-4 left-4 flex items-center gap-1 rounded-lg bg-lime-50 px-2 py-0.5 text-[0.8rem] text-lime-800"
+        class="absolute bottom-2 left-2 flex items-center gap-1 rounded-lg bg-lime-50 px-2 py-0.5 text-[0.8rem] text-lime-800"
       >
         <IconMapPin class="!size-4" /> {{ city }}
       </div>
     </div>
-    <div class="flex flex-col gap-4 p-4">
+    <div class="flex grow flex-col justify-between gap-4 p-4">
       <div class="flex flex-col gap-1">
         <h3 class="font-bold">{{ title }}</h3>
         <p class="line-clamp-3 text-sm opacity-70">{{ description }}</p>
       </div>
-      <div class="flex items-center justify-between gap-2">
-        <div class="flex items-center gap-2 text-xs">
-          <div class="flex items-center gap-1">
-            <IconSquareDashed class="!size-4 text-lime-700 dark:text-lime-200" />{{
-              n(surface, 'square-meter')
-            }}
-          </div>
-          <div class="flex items-center gap-1">
-            <IconMountain class="!size-4 text-lime-700 dark:text-lime-200" />{{
-              n(altitude, 'meter')
-            }}
-          </div>
+      <div class="flex items-center gap-4 text-xs">
+        <div class="flex items-center gap-1">
+          <IconSquareDashed class="!size-4 text-lime-700 dark:text-lime-200" />{{
+            n(surface, 'square-meter')
+          }}
         </div>
-        <Button
-          size="sm"
-          class="text-lime-700 dark:text-lime-200"
-          variant="ghost"
-          >Détails <IconArrowRight
-        /></Button>
+        <div class="flex items-center gap-1">
+          <IconMountain class="!size-4 text-lime-700 dark:text-lime-200" />{{
+            n(altitude, 'meter')
+          }}
+        </div>
       </div>
     </div>
   </div>
@@ -60,7 +54,6 @@
 
 <script setup lang="ts">
 import type { LandProposal } from '.';
-import IconArrowRight from '@lychen/vue-icons/IconArrowRight.vue';
 import IconHeart from '@lychen/vue-icons/IconHeart.vue';
 import IconHeartFilled from '@lychen/vue-icons/IconHeartFilled.vue';
 import IconMapPin from '@lychen/vue-icons/IconMapPin.vue';
--- projects/espace/website/src/views/home/ViewHome.vue
+++ projects/espace/website/src/views/home/ViewHome.vue
@@ -15,13 +15,14 @@
     </div>
     <div>Map</div>
   </Container>
-  <Container>
+  <Container class="flex flex-col items-center justify-center gap-4 text-center">
     <Title variant="h2">Derniers terrains disponibles</Title>
     <div class="flex flex-row gap-4">
       <CardLandProposal
         v-for="item in fakeLandProposals"
         :key="item.uuid"
         v-bind="item"
+        display-city
       />
     </div>
   </Container>
@@ -29,7 +30,10 @@
 
 <script lang="ts" setup>
 import { useI18nExtended } from '@lychen/vue-i18n/composables/useI18nExtended';
-import { Card as CardLandProposal } from '@lychen/vue-components-business/land-proposal/card';
+import {
+  Card as CardLandProposal,
+  type LandProposal,
+} from '@lychen/vue-components-business/land-proposal/card';
 import { MESSAGES, TRANSLATION_KEY } from './i18n';
 import { Container } from '@lychen/vue-components-website/container';
 import { Title } from '@lychen/vue-components-website/title';
@@ -40,15 +44,44 @@ import IconSearch from '@lychen/vue-icons/IconSearch.vue';
 
 const { t } = useI18nExtended({ messages: MESSAGES, rootKey: TRANSLATION_KEY, prefixed: true });
 
-const fakeLandProposals = [
+const fakeLandProposals: LandProposal[] = [
   {
     uuid: '1',
+    title: 'Terrain de culture',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec metus vel ante feugiat finibus. Nullam nec metus vel ante feugiat finibus.',
+    surface: 130,
+    altitude: 678,
+    city: 'Lille',
+    image: 'https://images.pexels.com/photos/59321/pexels-photo-59321.jpeg',
   },
   {
     uuid: '2',
+    title: 'Cave à champignons',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec metus vel ante feugiat finibus. Nullam nec metus vel ante feugiat finibus.',
+    surface: 40,
+    altitude: 150,
+    city: 'Toulouse',
+    image: 'https://images.pexels.com/photos/2499862/pexels-photo-2499862.jpeg',
   },
   {
     uuid: '3',
+    title: 'Fleurs et arbres',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    surface: 75,
+    altitude: 0,
+    city: 'Lyon',
+    image: 'https://images.pexels.com/photos/131772/pexels-photo-131772.jpeg',
+  },
+  {
+    uuid: '4',
+    title: 'Terrain de culture',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    surface: 37,
+    altitude: 345,
+    city: 'Grenoble',
+    image: 'https://images.pexels.com/photos/1084540/pexels-photo-1084540.jpeg',
   },
 ];
 </script>
