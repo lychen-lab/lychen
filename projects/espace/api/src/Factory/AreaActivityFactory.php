0934eb7b91115a33899a449fa43e4d0f3d954131
Nathan De Pachtere <nathan.depachtere@alpsify.com>
2025-12-31 18:00:10 +0100

    Create factories

---
+++ projects/espace/api/src/Factory/AreaActivityFactory.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Factory;
+
+use App\Entity\AreaActivity;
+use Override;
+use Zenstruck\Foundry\Persistence\PersistentObjectFactory;
+
+/**
+ * @extends PersistentObjectFactory<AreaActivity>
+ */
+final class AreaActivityFactory extends PersistentObjectFactory
+{
+    #[Override]
+    public static function class(): string
+    {
+        return AreaActivity::class;
+    }
+
+    #[Override]
+    protected function defaults(): array|callable
+    {
+        return [
+            'code' => self::faker()->text(255),
+        ];
+    }
+
+    #[Override]
+    protected function initialize(): static
+    {
+        return $this;
+    }
+}
---
+++ projects/espace/api/src/Factory/AreaProposalFactory.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Factory;
+
+use App\Entity\AreaProposal;
+use Override;
+use Zenstruck\Foundry\Persistence\PersistentObjectFactory;
+
+/**
+ * @extends PersistentObjectFactory<AreaProposal>
+ */
+final class AreaProposalFactory extends PersistentObjectFactory
+{
+    #[Override]
+    public static function class(): string
+    {
+        return AreaProposal::class;
+    }
+
+    #[Override]
+    protected function defaults(): array|callable
+    {
+        return [
+            'description' => self::faker()->text(),
+            'state' => self::faker()->text(255),
+            'title' => self::faker()->text(255),
+        ];
+    }
+
+    #[Override]
+    protected function initialize(): static
+    {
+        return $this;
+    }
+}
