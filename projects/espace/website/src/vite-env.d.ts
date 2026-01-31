ba798c11150b5639f9a8bcb605d72f4fcedbb638
Nathan De Pachtere <nathan.depachtere@alpsify.com>
2026-01-31 12:27:23 +0100

    TExt with SSG project

--- projects/espace/website/src/vite-env.d.ts
+++ projects/espace/website/src/vite-env.d.ts
@@ -3,6 +3,7 @@
 interface ImportMetaEnv {
   readonly VITE_APP_ID: string;
   readonly VITE_UNHEAD_HOST: string;
+  readonly VITE_ESPACE_APP_HOST: string;
 }
 
 interface ImportMeta {
--- projects/website/vite.config.ts
+++ projects/website/vite.config.ts
@@ -6,15 +6,20 @@ import vueDevTools from 'vite-plugin-vue-devtools';
 import type { UserConfig } from 'vite';
 import type { ViteSSGOptions } from 'vite-ssg';
 import { ssgOptions } from '@lychen/vite-ssg/ssgOptions';
+import EnvRuntime from 'vite-plugin-env-runtime';
 
 const config: UserConfig & ViteSSGOptions = {
   server: {
     https: {},
     port: 5140,
   },
+  define: {
+    'window.__PRODUCTION__APP__CONF__': JSON.stringify({}),
+  },
   plugins: [
     vueDevTools(),
     tailwindcss(),
+    EnvRuntime(),
     mkcert({
       hosts: [process.env.VITE_UNHEAD_HOST || 'localhost'],
     }),
