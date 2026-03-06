65fb8572c0c49ef12e2764d47bbcba5af5e74e11
Nathan De Pachtere <nathan.depachtere@alpsify.com>
2026-03-06 10:35:02 +0100

    fix(eslint): remove eslint-plugin-tailwindcss incompatible with tailwindcss v4

--- eslint.config.js
+++ eslint.config.js
@@ -9,7 +9,6 @@ import markdown from '@eslint/markdown';
 import checkFile from 'eslint-plugin-check-file';
 import eslintPluginYml from 'eslint-plugin-yml';
 import { defineConfig } from 'eslint/config';
-import tailwind from 'eslint-plugin-tailwindcss';
 import path from 'node:path';
 
 export default defineConfig([
@@ -45,14 +44,6 @@ export default defineConfig([
   importPlugin.flatConfigs.recommended,
   ...markdown.configs.processor,
   ...eslintPluginYml.configs['flat/recommended'],
-  ...tailwind.configs['flat/recommended'],
-  {
-    settings: {
-      tailwindcss: {
-        config: false,
-      },
-    },
-  },
   {
     files: ['**/*.vue'],
     languageOptions: {
@@ -110,7 +101,6 @@ export default defineConfig([
       'yml/quotes': ['error', { prefer: 'single', avoidEscape: true }],
       'yml/sort-sequence-values': ['error', { order: { type: 'asc' }, pathPattern: '^dependsOn$' }],
       'yml/no-empty-sequence-entry': ['error'],
-      'tailwindcss/no-custom-classname': 'off',
     },
   },
 ]);
