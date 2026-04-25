cce09c533b34d446b7315827b615a33405705e68
Luc Auvigne <luc.auvigne@gmail.com>
2026-04-25 17:58:35 +0200

    Change translation

--- projects/espace/website/src/App.vue
+++ projects/espace/website/src/App.vue
@@ -9,11 +9,16 @@ import { usePreferredColorScheme } from '@lychen/vue-color-scheme/composables/us
 import { TooltipProvider } from '@lychen/vue-components-core/tooltip';
 import { defineOrganization, defineWebPage, defineWebSite } from '@unhead/schema-org';
 import { useHead } from '@unhead/vue';
+import { useI18nExtended } from '@lychen/vue-i18n/composables/useI18nExtended';
 
 usePreferredColorScheme();
+const { locale } = useI18nExtended();
 const title = 'espace';
 useHead({
   titleTemplate: `${title} | %s`,
+  htmlAttrs: {
+    lang: locale.value,
+  },
   templateParams: {
     schemaOrg: {
       host: import.meta.env.VITE_UNHEAD_HOST,
